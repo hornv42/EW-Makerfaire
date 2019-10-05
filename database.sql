2f531aa94616632eced7ee01ba514b1dacb7a843
Vikki Horn <36120701+hornv42@users.noreply.github.com>
2019-10-04 20:34:04 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "stations" (
 	"stationID"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"name"	TEXT,
+	"name"		TEXT,
 	"question"	TEXT,
 	"answer"	TEXT,
-	"x_val"	REAL,
-	"y_val"	REAL
+	"x_val"		REAL,
+	"y_val"		REAL
 );
 CREATE TABLE IF NOT EXISTS "results" (
 	"sessionID"	INTEGER,
